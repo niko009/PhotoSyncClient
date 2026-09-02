b664ce577875bdf0fafb14e0950bef827d0722cc
Mihail <niko009@mail.ru>
2026-09-02 12:27:29 +0300

    feat: track remote sharing metadata on folder details

--- android/app/src/main/java/com/photosync/android/domain/model/FolderDetail.kt
+++ android/app/src/main/java/com/photosync/android/domain/model/FolderDetail.kt
@@ -4,4 +4,11 @@ data class FolderDetail(
     val id: String,
     val name: String,
     val photos: List<PhotoItem>,
-)
+    val remoteAlbumId: Int? = null,
+    val permission: String = "Owner",
+    val sharingMode: String = "Private",
+    val ownedByMe: Boolean = true,
+) {
+    val canContribute: Boolean
+        get() = ownedByMe || permission.equals("Contribute", ignoreCase = true) || permission.equals("Owner", ignoreCase = true)
+}
