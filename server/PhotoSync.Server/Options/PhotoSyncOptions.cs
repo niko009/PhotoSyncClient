7f15926b67035d861436ed869c2a7b0bfef0f52a
Mihail <niko009@mail.ru>
2026-09-03 19:54:18 +0300

    chore: remove obsolete device enrollment limits

--- server/PhotoSync.Server/Options/PhotoSyncOptions.cs
+++ server/PhotoSync.Server/Options/PhotoSyncOptions.cs
@@ -13,8 +13,6 @@ public sealed class PhotoSyncOptions
     public string PreviewRoot { get; set; } = Path.Combine("data", "previews");
 
     public string DatabasePath { get; set; } = Path.Combine("data", "system", "photosync.db");
-    public bool AllowDeviceRegistration { get; set; } = true;
-    public int MaxDevices { get; set; } = 5;
     public long MaxFileBytes { get; set; } = 25 * 1024 * 1024;
     public long MaxStorageBytes { get; set; } = 10L * 1024 * 1024 * 1024;
     public long MinFreeDiskBytes { get; set; } = 512L * 1024 * 1024;
